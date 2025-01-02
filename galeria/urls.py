597b00082f0580fd1c2bd8c7e9efc050e5013bea
Deivison-TI <deivison.ti@alpina.com.br>
2025-01-02 17:00:20 -0300

    Created a class in models.py to make the control of posts in this gallery of space photos by storing them on DB, turning possible to categorize and consequently, easier to search for them in the site. Also made the admin side to be able to insert and publish posts in the gallery of space photos

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 app_name='galeria'
 
 urlpatterns=[
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem'),
+    path('imagem/<int:fotografia_id>/', imagem, name='imagem'),
+    path('buscar/', buscar, name='buscar'),
 ]
